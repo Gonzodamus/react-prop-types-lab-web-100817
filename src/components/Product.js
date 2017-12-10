64ffabcffa47d3e8c5797cfdb08a45119b152c33
Gonzodamus <Dick@TheDickWard.com>
2017-12-10 13:37:12 -0500

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,41 @@
-// Code Product Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Product extends React.Component {
+  render() {
+    return (
+      <ul>
+      <li>Name: {this.props.name}</li>
+      <li>Producer: {this.props.producer}</li>
+      <li>Watermark: {this.props.hasWatermark.toString()}</li>
+      <li>Color: {this.props.color}</li>
+      <li>Weight: {this.props.weight}</li>
+      </ul>
+    )
+  }
+}
+
+let weight = []
+
+function makeWeightArray (){
+  for (let i = 80; i <= 300; i++){
+  weight.push(i)
+  }
+}
+
+makeWeightArray()
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+  weight: PropTypes.oneOf(weight).isRequired
+}
+
+
+Product.defaultProps = {
+  hasWatermark: false
+}
+
+export default Product
